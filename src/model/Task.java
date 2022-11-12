2d62b87f263cda14bca8a03f7f7aa9347e8e6bc4
DharmaHS <dharma.siemarga@gmail.com>
2022-11-13 00:12:22 +0700

    added Task variable, setter and getter

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,5 +1,38 @@
 package model;
 
+import java.util.*;
 public class Task {
     
+    private String name;
+    private boolean isDone;
+    private Date dueDate;
+    private boolean isImportant;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    public Date getDueDate() {
+        return this.dueDate;
+    }
+
+    public void setImportance(boolean importance) {
+        this.isImportant = importance;
+    }
+    public boolean getImportance() {
+        return this.isImportant;
+    }
 }
