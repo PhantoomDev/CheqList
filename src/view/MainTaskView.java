c68e086e0253f5c0e28322fc03db15286dfa8fa8
DharmaHS <dharma.siemarga@gmail.com>
2022-11-13 02:34:34 +0700

    added some basic component for MainTaskView.java

---
+++ src/view/MainTaskView.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class MainTaskView extends JPanel{
+
+    private JToggleButton checkButton = new JToggleButton();
+    private JToggleButton importanceButton = new JToggleButton();
+    private JTextField taskTextField = new JTextField();
+    private JLabel dateLabel = new JLabel();
+}
